2c77b16b4655e68b079ac5a8800f12e818b114aa
Jasmine Dahilig <jasmine.dahilig@gmail.com>
2014-02-18 05:12:49 -0800

    playing around with haml in the index view; its not cooperating

--- web-client/app.js
+++ web-client/app.js
@@ -4,79 +4,81 @@
 
 var express = require('express');
 var pg = require('pg');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
 
-var app = module.exports = express.createServer();
-
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'haml');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-app.set('view options', {
-  layout: false
-});
+var app = express();
 
 // Use hamljs for HAML views
-app.register('.haml', require('hamljs'));
+app.engine('.haml', require('hamljs').renderFile);
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'haml');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
 
 // Routes
-app.get('/', function(req, res){
-  res.render('index', {
-  });
-});
+app.get('/', routes.index);
+app.get('/users', user.list);
 
 app.post('/RetrieveCadastre', function(req, res){
-    RetrieveCadastre(req.body, res);
+  RetrieveCadastre(req.body, res);
 });
 
 // RetrieveCadastre
 function RetrieveCadastre(bounds, res){
 
-    var connString = 'tcp://spatial:spatial@localhost/Spatial';
+  var connString = 'tcp://spatial:spatial@localhost/Spatial';
 
-    pg.connect(connString, function(err, client) {
+  pg.connect(connString, function(err, client) {
 
-        var sql = 'select ST_AsGeoJSON(geog) as shape ';
-        sql = sql + 'from spatial.state_1 ';
-        sql = sql + 'where geog && ST_GeogFromText(\'SRID=4326;POLYGON(($1 $2,$3 $4,$5 $6,$7 $8,$9 $10))\') ';
-        sql = sql + 'and ST_Intersects(geog, ST_GeogFromText(\'SRID=4326;POLYGON(($11 $12,$13 $14,$15 $16,$17 $18,$19 $20))\'));';
+    var sql = 'select ST_AsGeoJSON(geog) as shape ';
+    sql = sql + 'from spatial.state_1 ';
+    sql = sql + 'where geog && ST_GeogFromText(\'SRID=4326;POLYGON(($1 $2,$3 $4,$5 $6,$7 $8,$9 $10))\') ';
+    sql = sql + 'and ST_Intersects(geog, ST_GeogFromText(\'SRID=4326;POLYGON(($11 $12,$13 $14,$15 $16,$17 $18,$19 $20))\'));';
 
-        var vals = [bounds._southWest.lng, bounds._southWest.lat, bounds._northEast.lng, bounds._southWest.lat, bounds._northEast.lng, bounds._northEast.lat, bounds._southWest.lng, bounds._northEast.lat, bounds._southWest.lng, bounds._southWest.lat];
-        var vals = vals.concat(vals);
+    var vals = [
+      bounds._southWest.lng, bounds._southWest.lat, bounds._northEast.lng,
+      bounds._southWest.lat, bounds._northEast.lng, bounds._northEast.lat,
+      bounds._southWest.lng, bounds._northEast.lat, bounds._southWest.lng, bounds._southWest.lat
+    ];
+    var vals = vals.concat(vals);
 
-        client.query(sql, vals, function(err, result) {
-            var featureCollection = new FeatureCollection();
+    client.query(sql, vals, function(err, result) {
+      var featureCollection = new FeatureCollection();
 
-            for (i = 0; i < result.rows.length; i++)
-            {
-                featureCollection.features[i] = JSON.parse(result.rows[i].shape);
-            }
+      for (i = 0; i < result.rows.length; i++)
+      {
+          featureCollection.features[i] = JSON.parse(result.rows[i].shape);
+      }
 
-            res.send(featureCollection);
-        });
+      res.send(featureCollection);
     });
+  });
+
 }
 
 // GeoJSON Feature Collection
 function FeatureCollection(){
-    this.type = 'FeatureCollection';
-    this.features = new Array();
+  this.type = 'FeatureCollection';
+  this.features = new Array();
 }
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
