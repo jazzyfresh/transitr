891f96396e926c853239e980f587cb3925a53531
Jasmine Dahilig <jasmine.dahilig@gmail.com>
2014-05-05 09:10:55 -0700

    prepped app for heroku

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ var express = require('express'),
     path = require('path'),
     pg = require('pg').native,
     connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/transitr',
-    start = new Date(),
     client;
 
 client = new pg.Client(connectionString);
@@ -24,6 +23,30 @@ app.use(app.router);
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
+
+app.get('/', function(req, res) {
+  var stopString = "";
+  query = client.query("select column_name from information_schema.columns where table_name='stops'");
+  query.on('row', function(row, result) {
+    console.log(result);
+    result.addRow(row.column_name);
+  });
+  query.on('end', function(result) {
+    if (!result) res.send('No data found');
+    console.log(result.rows.join());
+    stopString = "Stop columns: " + result.rows.join(", ");
+    query = client.query("select column_name from information_schema.columns where table_name='routes'");
+    query.on('row', function(row, result) {
+      console.log(result);
+      result.addRow(row.column_name);
+    });
+    query.on('end', function(result) {
+      if (!result) res.send('No data found');
+      console.log(result.rows.join());
+      res.send(stopString + ". " + "Route columns: " + result.rows.join(", "));
+    });
+  });
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- server/schema.md
+++ server/schema.md
@@ -1,30 +1,33 @@
-## LA Metro Data   
 
-###Route:   
-  id: int   
-  display_name: String   
-  bg_color: String   
-  fg_color: String   
+    LA Metro Data
+    -------------
 
-###Stops:   
-  id: int   
-  display_name: String   
-  latitude: float   
-  longitude: float   
+    Route:
+      id: int
+      display_name: String
+      bg_color: String
+      fg_color: String
 
-## Transitr Data   
+    Stops:
+      id: int
+      display_name: String
+      latitude: float
+      longitude: float
 
-###User:   
-  id: int   
-  first_name: String   
-  last_name: String   
-  email: String   
+    Transitr Data
+    -------------
 
-###Status:   
-  id: int   
-  user_id: int   
-  route_id: int   
-  stop_id: int   
-  text: String   
-  date_time: Datetime   
+    User:
+      id: int
+      first_name: String
+      last_name: String
+      email: String
+
+    Status:
+      id: int
+      user_id: int
+      route_id: int
+      stop_id: int
+      text: String
+      date_time: Datetime
 
